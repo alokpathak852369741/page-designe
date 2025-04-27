65f894c1b9309b035485fb809edb9aa138eefd7c
alokpathak852369741 <alokpathak852369741@gmail.com>
2025-04-27 12:22:51 +0530

    Update script.js

--- page-designe/script.js
+++ page-designe/script.js
@@ -8,3 +8,13 @@ function scroller() {
   });
 }
 scroller();
+function scroller() {
+  $(".smooth").on("scroll", function() {
+    let scrollPos = $(this).scrollTop();
+    $(".parallax").css({
+      top: scrollPos / 2 + "px",
+      opacity: 1 - scrollPos / 200
+    });
+  });
+}
+scroller();
